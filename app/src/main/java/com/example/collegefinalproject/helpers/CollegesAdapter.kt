bf9185ad3679fcf7494d594e69709006e1678e77
Christopher Kuizon <christopher.kuizon@gmail.com>
2021-05-14 11:29:30 -0700

    Third API Commit

---
+++ app/src/main/java/com/example/collegefinalproject/helpers/CollegesAdapter.kt
@@ -0,0 +1,59 @@
+package dev.bensalcie.retrofitest.helpers
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.collegefinalproject.R
+import com.example.collegefinalproject.mainMenu.MainActivity
+import com.example.collegefinalproject.models.College
+import com.squareup.picasso.Picasso
+
+class CollegesAdapter(private val collegesList: List<College>) :RecyclerView.Adapter<CollegesAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val view  = LayoutInflater.from(parent.context).inflate(R.layout.fragment_college,parent,false)
+        return ViewHolder(view)
+    }
+
+
+    override fun getItemCount(): Int {
+
+        return collegesList.size
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Log.d("Response", "List Count :${collegesList.size} ")
+
+
+        return holder.bind(collegesList[position])
+
+    }
+    class ViewHolder(itemView : View) :RecyclerView.ViewHolder(itemView) {
+
+        var nameOfCollege = itemView.findViewById<TextView>(R.id.textView_college_collegeName)
+        var description = itemView.findViewById<TextView>(R.id.textView_college_description)
+        var filter = MainActivity.filter
+        fun bind(college: College) {
+
+            nameOfCollege.text = college.school.name
+            when {
+                filter.contains("cost") -> {
+                    description.text = college.latest.cost.tuition.in_state.toString()
+                }
+                filter.contains("debt") -> {
+                    description.text = college.latest.aid.cumulative_debt.number.toString()
+                }
+                filter.contains("Asian") -> {
+                    description.text = college.latest.student.demographics.race_ethnicity.asian.toString()
+                }
+            }
+        }
+
+    }
+}
